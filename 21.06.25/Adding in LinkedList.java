ad00619276d4640930a70cd818efa12d5fd192fc
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2025-06-21 20:08:35 +0530

    Create Adding in LinkedList.java

---
+++ 21.06.25/Adding in LinkedList.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+class Main {
+  public static void main(String[] args){
+    LinkedList<String> languages = new LinkedList<>();
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("JavaScript");
+    System.out.println("LinkedList: " + languages);
+  }
+}
