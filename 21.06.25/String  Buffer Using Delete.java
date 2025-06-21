b9d736cabf11cd3ec620b20869d2778876ee4b2c
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2025-06-21 20:06:17 +0530

    Create String  Buffer Using Delete.java

---
+++ 21.06.25/String  Buffer Using Delete.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    StringBuffer str = new StringBuffer();
+    str.append("Java");
+    str.append(" is");
+    str.append(" popular.");
+    System.out.println("StringBuffer: " + str);
+    str.delete(0, str.length());
+    System.out.println("Updated StringBuffer: " + str);
+  }
+}
