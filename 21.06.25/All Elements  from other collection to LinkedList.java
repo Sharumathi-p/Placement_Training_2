b115f5b35a67c2f05da015e7cd09636329abaea3
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2025-06-21 20:29:59 +0530

    Create All Elements  from other collection to LinkedList.java

---
+++ 21.06.25/All Elements  from other collection to LinkedList.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+class Main {
+  public static void main(String[] args) {
+    LinkedList<String> mammals = new LinkedList<>();
+    mammals.add("Dog");
+    mammals.add("Cat");
+    mammals.add("Horse");
+    System.out.println("Mammals: " + mammals);
+    LinkedList<String> animals = new LinkedList<>();
+    animals.add("Crocodile");
+    animals.addAll(mammals);
+    System.out.println("Animals: " + animals);
+  }
+}
