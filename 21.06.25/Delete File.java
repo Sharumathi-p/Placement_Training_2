5b0d789601163a524ca995dba112f481d246a304
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2025-06-21 19:56:20 +0530

    Create Delete File.java

---
+++ 21.06.25/Delete File.java
@@ -0,0 +1,13 @@
+import java.io.File;
+class Main {
+  public static void main(String[] args) {
+    File file = new File("JavaFile.java");
+    boolean value = file.delete();
+    if(value) {
+      System.out.println("JavaFile.java is successfully deleted.");
+    }
+    else {
+      System.out.println("File doesn't exit");
+    }
+  }
+}
