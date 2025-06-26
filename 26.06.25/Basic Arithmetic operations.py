87c06c3820577c67d4c4a5312e4f25b3b5d09d3e
Sharumathi P <154426223+Sharumathi-p@users.noreply.github.com>
2025-06-26 19:34:04 +0530

    Create Basic Arithmetic operations.py

---
+++ 26.06.25/Basic Arithmetic operations.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+division_result = a / b
+print(sum_result)
+print(difference_result)
+print(product_result)
+print(division_result)
